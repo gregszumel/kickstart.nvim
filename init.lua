bf325f5e384d3c5e6e90c7ee9f23b87421cfdf5d
Greg Szumel <gszumel@relaypro.com>
2025-03-04 08:48:26 -0500

    color and avante

--- init.lua
+++ init.lua
@@ -890,21 +890,20 @@ require('lazy').setup({
     -- change the command in the config to whatever the name of that colorscheme is.
     --
     -- If you want to see what colorschemes are already installed, you can use `:Telescope colorscheme`.
-    'folke/tokyonight.nvim',
-    priority = 1000, -- Make sure to load this before all the other start plugins.
-    config = function()
-      ---@diagnostic disable-next-line: missing-fields
-      require('tokyonight').setup {
-        styles = {
-          comments = { italic = false }, -- Disable italics in comments
-        },
-      }
-
-      -- Load the colorscheme here.
-      -- Like many other themes, this one has different styles, and you could load
-      -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
-      vim.cmd.colorscheme 'tokyonight-night'
-    end,
+    --
+    {
+      'sainnhe/everforest',
+      lazy = false,
+      priority = 1000,
+      config = function()
+        -- Optionally configure and load the colorscheme
+        -- directly inside the plugin declaration.
+        vim.g.everforest_enable_italic = 1
+        vim.g.background = 'dark'
+        vim.g.everforest_background = 'medium'
+        vim.cmd.colorscheme 'everforest'
+      end,
+    },
   },
 
   -- Highlight todo, notes, etc in comments
